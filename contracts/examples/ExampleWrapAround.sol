7431fd28f4061d8d68e002fb63c9bba75f741557
cibss <cibss13@gmail.com>
2026-05-28 19:08:43 +0700

    feat: basic solidity

---
+++ contracts/examples/ExampleWrapAround.sol
@@ -0,0 +1,19 @@
+//SPDX-License-Identifier: MIT
+ 
+pragma solidity 0.8.15;
+ 
+contract ExampleWrapAround {
+    uint256 public myUint;
+ 
+    function decrementUintUnchecked() public {
+        unchecked {
+            myUint--;
+        }
+    }
+ 
+    function decrementUint() public {
+        myUint--;
+    }
+}
+
+
